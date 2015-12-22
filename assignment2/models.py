88b0b8ec13dc13750c50e3b291f4218acc27728e
sanirim <sametyilmaz@std.sehir.edu.tr>
2015-12-22 08:36:23 +0200

    Initial commit

---
+++ assignment2/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20)
+    classroom = models.CharField(max_length=30)
+    times = models.TimeField()
+    teacher = models.ForeignKey(teacher)
+    students = models.ManyToManyField(student, blank=True)
+
+class teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    office_details = models.CharField(max_length=200)
+    phone = models.IntegerField()
+    email = models.EmailField()
+
+class student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    courses = models.ManyToManyField(course, through=course.students.through, blank=True)
